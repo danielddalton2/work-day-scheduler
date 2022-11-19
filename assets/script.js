d08aec40b09ce477bbe62ee8f14a4e3392f2c399
danielddalton2 <danielddalton2@gmail.com>
2022-11-19 06:34:40 -0800

    formatted code

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,22 @@
 $(document).ready(function () {
   $(".saveBtn").click(function () {
     console.log($(this));
-  var time=$(this).parent().attr("id");
-  console.log(time);
-  var text=$(this).siblings("textarea").val();
-  console.log(text);
-  localStorage.setItem(time, text);
-  })
-
-
+    var time = $(this).parent().attr("id");
+    console.log(time);
+    var text = $(this).siblings("textarea").val();
+    console.log(text);
+    localStorage.setItem(time, text);
+  });
 
   $(".time-block").each(function () {
     console.log(this);
-    var id=$(this).attr("id");
+    var id = $(this).attr("id");
     console.log(id);
     var savedText = localStorage.getItem(id);
     $(this).children("textarea").val(savedText);
-  })
+  });
 
   $("#currentDay").text(dayjs().format("[Today is] dddd, MMMM DD, YYYY"));
+});
 
 // TODO: Color coordinate the time
-
-// TODO: Add a message that shows when something is saved 
-
-});
