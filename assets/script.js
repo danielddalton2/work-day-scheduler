afbdf511f0eb0c2ba25108a76dd3fc8958a4815e
danielddalton2 <danielddalton2@gmail.com>
2022-11-19 14:43:08 -0800

    added colors to background of time-blocks

--- assets/script.js
+++ assets/script.js
@@ -16,43 +16,31 @@ $(document).ready(function () {
     $(this).children("textarea").val(savedText);
   });
 
+  $("#currentDay").text(dayjs().format("[Today is] dddd, MMMM DD, YYYY"));
+
   function timeHighlight() {
     var currentTime = dayjs().hour();
     console.log(currentTime);
-  
-    // iterate through each time block
-    $('.time-block').each(function() {
-        var blockTime = parseInt($(this).attr('id').split('-')[1]);
-        console.log(blockTime);
-  
-        if (blockTime < currentTime) {
-            $(this).addClass('past');
-        } else if (blockTime === currentTime) {
-            $(this).removeClass('past');
-            $(this).addClass('present');
-        } else {
-            $(this).removeClass('past');
-            $(this).removeClass('present');
-            $(this).addClass('future');
-        }
+
+    $(".time-block").each(function () {
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+      console.log(blockTime);
+
+      if (blockTime < currentTime) {
+        $(this).addClass("past");
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+      } else if (blockTime === currentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
     });
   }
 
-  $('#9 .description').val(localStorage.getItem('9'));
-  $('#10 .description').val(localStorage.getItem('10'));
-  $('#11 .description').val(localStorage.getItem('11'));
-  $('#12 .description').val(localStorage.getItem('12'));
-  $('#13 .description').val(localStorage.getItem('13'));
-  $('#14 .description').val(localStorage.getItem('14'));
-  $('#15 .description').val(localStorage.getItem('15'));
-  $('#16 .description').val(localStorage.getItem('16'));
-  $('#17 .description').val(localStorage.getItem('17'));
-  
   timeHighlight();
-
-
-  $("#currentDay").text(dayjs().format("[Today is] dddd, MMMM DD, YYYY"));
-
-
 });
-
